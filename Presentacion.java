2cd4810f18362995d86dfddcd7a9b0863f813f5e
Kylver Paucar <kilverpaucar1@gmail.com>
2025-10-03 09:34:48 -0500

    feat: agregar archivo base de presentación

---
+++ Presentacion.java
@@ -0,0 +1,6 @@
+public class Presentacion {
+    public static void main(String[] args) {
+        System.out.println("Integrante 1: [Nombre]");
+        System.out.println("Integrante 2: [Nombre]");
+    }
+}
